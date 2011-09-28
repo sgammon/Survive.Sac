d3ab7087c6c80ff5ad088be3378cd0d63be0dc54
Sam Gammon <sgammon@bluestatedigital.com>
2011-09-27 23:33:03 -0700

    Installed Jinja2 and webapp2_extras, split HTML from inline out to templates, fixed some CSS bugs (thanks @davidrekow).

--- teammacdown.py
+++ teammacdown.py
@@ -2,11 +2,14 @@
 
 import cgi
 import urllib
+import config
 import datetime
 
 import logging
 
 import webapp2 as webapp
+
+from webapp2_extras import jinja2
 
 from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -30,21 +33,30 @@ def team_key(team_name=None):
 	return db.Key.from_path('Team', team_name or 'default_team')
 
 
-class MainPage(webapp.RequestHandler):
+class TMDHandler(webapp.RequestHandler):
+	
+	@webapp.cached_property
+	def jinja2(self):
+		return jinja2.get_jinja2(app=self.app)
+		
+	def render(self, template, context={}, **kwargs):
+		
+		if len(kwargs) > 0:
+			for k, v in kwargs.items():
+				context[k] = v
+		
+		self.response.write(self.jinja2.render_template(template, **context))
+
+
+class MainPage(TMDHandler):
 
 	def get(self):
 		
-		self.response.write("""
-<html>
-	<head>
-		<title>Register to play Survive Sac 2011</title>
-		<link rel="stylesheet" type="text/css" href="style/survive-sac-v1.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
-	</head>
-	<body><div id='main'>""")
-	
 		fail = self.request.get('fail', False)
 		success = self.request.get('success', False)
+
+		alert = False
+		alert_type = False
 
 		if any([fail, success]):
 
@@ -53,55 +65,25 @@ class MainPage(webapp.RequestHandler):
 			else:
 				alert_type = 'success'
 				
-			self.response.write("""
-				<div id='alert' class='%s'><h2>
-			""" % alert_type)
-
 			if fail is not False:
 				fail = self.request.get('reason')
 				if fail == 'unknown':
-					self.response.write('An unknown error occurred. Please try again later.')
+					alert = 'An unknown error occurred. Please try again later.'
 				elif fail == 'paypal_invalid':
-					self.response.write('The PayPal verification you provided was invalid. Please try again.')
+					alert = 'The PayPal verification you provided was invalid. Please try again.'
 			elif success is not False:
-				self.response.write('Success! You have been registered.')
+				alert = 'Success! You have been registered.'
 	
-			self.response.write("""
-				</h2></div>
-			""")
-	
-		self.response.write("""<div id="wrapper">
-			<h1>REGISTER</h1>""")
-			
+				
 		team_name = self.request.get('team_name')
 		
-		players = db.GqlQuery("SELECT * FROM Player WHERE ANCESTOR IS :1 ORDER BY date DESC LIMIT 10", team_key(team_name))
+		#players = db.GqlQuery("SELECT * FROM Player WHERE ANCESTOR IS :1 ORDER BY date DESC LIMIT 10", team_key(team_name))
 		
-		self.response.write("""
-			<form action="/create?%s" method="post">
-				<div><input type="text" name="full_name" placeholder="name" value='name'/></div>
-				<div><input type="text" name="email" placeholder="email" value='email' /></div>
-				<div><input type="text" name="age" placeholder="age" value='age' /></div>
-				<div><textarea name="refer" id="refer" rows="6" placeholder="how did you hear about Survive Sac?">how did you hear about Survive Sac?</textarea></div>
-				<div><input type="text" name="previous_game" placeholder="did you play last year?" value='did you play last year?' /></div>
-				<div><input type="text" name="smartphone" placeholder="do you have a smartphone?" value='do you have a smartphone?' /></div>
-				<div><input type="text" name="paypal" placeholder="paypal confirmation number"  value='paypal confirmation number'/></div>
-				<div><input type="submit" id="submit" value="create player"/></div>
-			</form>
-		</div></div>
-	<script type="text/javascript">
-		$('input').click(function clearIfDefault(){
-			$(this).val('');
-		});
-		$('textarea').click(function clearIfDefault(){
-			$(this).val('');
-		});
-	</script>
-	</body>
-</html>""" % (urllib.urlencode({'team_name': team_name})))
+		self.render('registration.html', alert=alert, alert_type=alert_type)
+		
 
 
-class Team(webapp.RequestHandler):
+class Team(TMDHandler):
 
 	def post(self):
 
@@ -141,39 +123,20 @@ class Team(webapp.RequestHandler):
 		else:
 			self.redirect('/?' + urllib.urlencode({'fail': 'true', 'reason': 'unknown'}))
 			
-class Callback(webapp.RequestHandler):
+			
+class Callback(TMDHandler):
 	
 	def get(self):
 		
 		full_name = self.request.get('full_name')
-		
-		self.response.write("""
-<html>
-	<head>
-		<title>You have been registered.</title>
-		<link rel="stylesheet" type="text/css" href="style/survive-sac-v1.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
-	</head>
-	<body>
-		<div id='main'>
-			<div id="wrapper">
-				<h1>SUCCESS</h1>
-				<p>You have successfully registered player <strong>%s</strong> in Survive Sacramento 2011: Outbreak. Confirmation has been sent to the email address you gave us.</p>
-				<p>Future information about the game, including game specifics and previews of game surprises, will be sent via email, and on Facebook if you chose to connect.</p>
-				<p>Thanks for making this game a reality, and we'll see you at the briefing.</p>
-				<p style="margin-left: 50px">- &laquo;TMD&raquo
-			</div>
-		</div>
-	</body>
-</html>""" % str(full_name))
-		
+		self.render('confirmation.html', fullname=full_name)
 		
 
 TMD = webapp.WSGIApplication([
 	('/', MainPage),
 	('/create', Team),
 	('/success', Callback)
-	], debug=True)
+	], debug=True, config=config.config)
 	
 	
 def main():
